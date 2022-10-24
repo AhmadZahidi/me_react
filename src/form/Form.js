9445af8a928b99bebcfc9d28d40c9ccd71fa6140
zahidi <zahidiazmi120@gmail.com>
2022-10-24 10:09:25 +0800

    add form gui

---
+++ src/form/Form.js
@@ -0,0 +1,21 @@
+const Form = () => {
+  return (
+    <form>
+      <div>
+        <label>Name:</label>
+        <input type="text" />
+
+        <label>Age:</label>
+        <input type="text" />
+
+        <label>Bio:</label>
+        <input type="text" />
+      </div>
+      <div>
+        <button>submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
