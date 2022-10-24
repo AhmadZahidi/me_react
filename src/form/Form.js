b07669eb887c5f119efe7ba213ec6d9c3f4d8311
zahidi <zahidiazmi120@gmail.com>
2022-10-24 10:12:41 +0800

    add event listener

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,15 +1,28 @@
 const Form = () => {
+
+    const nameChangeHandler=(event)=>{
+        console.log(event.target.value);
+    };
+
+    const ageChangeHandler=(event)=>{
+        console.log(event.target.value);
+    }
+
+    const bioChangeHandler=(event)=>{
+        console.log(event.target.value);
+    }
+
   return (
     <form>
       <div>
         <label>Name:</label>
-        <input type="text" />
+        <input type="text" onChange={nameChangeHandler}/>
 
         <label>Age:</label>
-        <input type="text" />
+        <input type="text" onChange={ageChangeHandler}/>
 
         <label>Bio:</label>
-        <input type="text" />
+        <input type="text" onChange={bioChangeHandler}/>
       </div>
       <div>
         <button>submit</button>
